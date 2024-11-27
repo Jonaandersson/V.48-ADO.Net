1ff96e7618656ea29f18ca64548cf061be2d0c5a
jonaa <jonaa@THINKPADJONA>
2024-11-27 15:19:26 +0100

    Add project files.

---
+++ V.48/DatabaseHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+
+
+namespace V._48
+{
+    
+
+    public class DatabaseHelper
+    {
+        private readonly string _connectionString;
+
+        public DatabaseHelper(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<Film> GetFilmsByActor(string actorName)
+        {
+            var films = new List<Film>();
+
+            string query = @"
+            SELECT f.film_id, f.title 
+            FROM actor AS a
+            JOIN film_actor AS fa ON a.actor_id = fa.actor_id
+            JOIN film AS f ON fa.film_id = f.film_id
+            WHERE a.first_name + ' ' + a.last_name = @actorName;
+        ";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@actorName", actorName);
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            films.Add(new Film
+                            {
+                                Id = Convert.ToInt32(reader["film_id"]),
+                                Title = reader["title"].ToString()
+                            });
+                        }
+                    }
+                }
+            }
+
+            return films;
+        }
+    }
+
+}
---
+++ V.48/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace V._48
+{
+
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.Data.SqlClient;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Sakila;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            var dbHelper = new DatabaseHelper(connectionString);
+
+            Console.Write("Ange skådespelarens namn (Personlig favvis är Elvis Marx): ");
+            string actorName = Console.ReadLine();
+
+            
+            List<Film> films = dbHelper.GetFilmsByActor(actorName);
+
+            if (films.Count > 0)
+            {
+                Console.WriteLine("\nFilmer där skådespelaren är delaktig:");
+                foreach (var film in films)
+                {
+                    Console.WriteLine($"- {film.Title}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Inga filmer hittades för den skådespelaren.");
+            }
+        }
+    }
+
+}
